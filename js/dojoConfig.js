a5a535ecc82072aa95ce742b94d7b82707020620
sylvenas <sylvenas@outlook.com>
2016-01-18 21:46:07 +0800

    修改本地引用地址和网络引用地址

--- js/dojoConfig.js
+++ js/dojoConfig.js
@@ -2,6 +2,7 @@
     parseOnLoad: true,
     packages: [{
         "name": "src",
-        "location": "/src"//for demo;location.pathname.replace(/\/[^/]+$/, '') + "/src"
+        "location": "/src"//for demo;本地引用地址
+        //"location": location.pathname.replace(/\/[^/]+$/, '') + "/src"    网络引用地址
     }]
 };
